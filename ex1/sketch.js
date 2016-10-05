c26fb40ade3b51207a18868d7d11b117b71500fd
talonendm <talonen.dm@gmail.com>
2016-10-05 21:29:55 +0300

    new ex1, homepage

--- ex1/sketch.js
+++ ex1/sketch.js
@@ -12,72 +12,112 @@
  
  
  */
-var bug;  // Declare object
-var canvas;
-var pmillis;
+var cnv;
+var dx = 700;
+var dy = 400;
+var sb = 5;
+var onlink = false;
+var numSegments = 14,
+  x = [],
+  y = [],
+  angle = [],
+  segLength = 26,
+  targetX, targetY;
+
+for (var i = 0; i < numSegments; i++) {
+  x[i] = 0;
+  y[i] = 0;
+  angle[i] = 0;
+}
+
+function centerCanvas() {
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+}
 
 function setup() {
-  // createCanvas(710, 400);
+  cnv = createCanvas(700, 400);
+  centerCanvas();
+  background(255, 0, 200);
+  strokeWeight(20);
+  stroke(255, 100);
 
-  canvas = createCanvas(window.innerWidth, window.innerHeight);
-  // Create object
-  bug = new Jitter();
-  pmillis = 0;
+  x[x.length-1] = width/2; // Set base x-coordinate
+  y[x.length-1] = height/2;  // Set base y-coordinate
+}
+
+function windowResized() {
+  centerCanvas();
 }
 
 function draw() {
-  //background(50, 89, 100);
-  stroke(110);
-  rect(0,200,width,height);
-  stroke(0);
-  bug.move();
-  bug.display();
-  text(frameCount, width/2, height/2);
-  var milli = millis();
-  text(milli - pmillis, width/2, height/2+20);
-  
-  line(frameCount % width,0,(frameCount+1) % width,(milli - pmillis)*4);
-  pmillis = milli;
-}
 
-window.onresize = function() {
-  var w = window.innerWidth;
-  var h = window.innerHeight;  
-  canvas.size(w,h);
-  width = w;
-  height = h;
-};
-
-// 160720
-// https://p5js.org/examples/demos/Hello_P5_Interactivity_1.php
-// When the user clicks the mouse
-function mousePressed() {
-  // Check if mouse is inside the circle
-  // var d = dist(mouseX, mouseY, 360, 200);
-  //if (d < 100) {
-    // Pick new random color values
-  //  r = random(255);
-  //  g = random(255);
-  //  b = random(255);
-  // }
-  bug = new Jitter();
-  
-}
+background(0,30,0);
+	fill(200);
 
 
-// Jitter class
-function Jitter() {
-  this.x = random(width);
-  this.y = random(height);
-  this.diameter = random(10, 100);
-  this.speed = 3;
-
-  this.move = function() {
-    this.x += random(-this.speed, this.speed);
-    this.y += random(-this.speed, this.speed);
-  };
-
-  this.display = function() {
-    ellipse(this.x, this.y, this.diameter, this.diameter);
+  if ((mouseX>60) && (mouseX<160) && (mouseY>53) && (mouseY<68)) {
+	onlink = true;
+  } else {
+	onlink = false;
   }
-};
+
+  if (onlink) {
+  	strokeWeight(4);
+  } else {
+	strokeWeight(2);
+  }
+  stroke(255, 100);
+   
+
+  if ((mouseIsPressed) && (onlink)) {
+	link("http://users.ics.aalto.fi/talonen/","_new");
+  }
+
+	text("jaakko.me",60,60);
+
+  strokeWeight(20);
+  stroke(255, 100);
+
+  reachSegment(0, mouseX, mouseY);
+  for(var i=1; i<numSegments; i++) {
+    reachSegment(i, targetX, targetY);
+  }
+  for(var j=x.length-1; j>=1; j--) {
+    positionSegment(j, j-1);
+  }
+  for(var k=0; k<x.length; k++) {
+    segment(x[k], y[k], angle[k], (k+1)*2);
+  }
+
+ stroke(0);
+ fill(0);
+ rect(0,0,dx,sb);
+ rect(0,dy-sb,dx,dy);
+ rect(0,0,sb,dy);
+ rect(dx-sb,0,dx,dy);
+
+}
+
+function positionSegment(a, b) {
+  x[b] = x[a] + cos(angle[a]) * segLength;
+  y[b] = y[a] + sin(angle[a]) * segLength;
+}
+
+function reachSegment(i, xin, yin) {
+  var dx = xin - x[i];
+  var dy = yin - y[i];
+  angle[i] = atan2(dy, dx);
+  targetX = xin - cos(angle[i]) * segLength;
+  targetY = yin - sin(angle[i]) * segLength;
+}
+
+function segment(x, y, a, sw) {
+  strokeWeight(sw);
+  push();
+  translate(x, y);
+  rotate(a);
+  line(0, 0, segLength, 0);
+  pop();
+}
